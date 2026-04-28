93aec7faf99e111c6e579e206e7c214c0c04b09e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-04-28 19:22:43 +0900

    feat: update Toaster import path to use sonner package

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Syne } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
